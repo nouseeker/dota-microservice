22c367e8dc15f618d411e5509aa0c9903d99517e
n7meless <n7meless@mail.ru>
2023-04-17 22:58:43 +0500

    add DateEnum.class and traversal by elements method

--- counter-service/src/main/java/dev/n7meless/counterservice/CounterServiceApplication.java
+++ counter-service/src/main/java/dev/n7meless/counterservice/CounterServiceApplication.java
@@ -2,8 +2,10 @@ package dev.n7meless.counterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CounterServiceApplication {
 
     public static void main(String[] args) {
